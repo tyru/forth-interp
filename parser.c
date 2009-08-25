76966e7af255ff7cf2121dc97d5eca00124d2e75
tyru <tyru.exe@gmail.com>
2009-08-25 22:43:12 +0900

    fix bug
    bug: parser does wrong parsing when leading whitespaces.

--- parser.c
+++ parser.c
@@ -11,6 +11,8 @@ forth_get_word_from_src(
         char *word,
         size_t max_size)
 {
+    int word_count;
+
     if (interp->src == NULL) goto ERR_ARGS;
     if (word == NULL) goto ERR_ARGS;
 
@@ -20,16 +22,19 @@ forth_get_word_from_src(
         if (is_word(interp->src[interp->cur_pos])) break;
         interp->cur_pos++;
     }
+
     // get word.
+    word_count = 0;
     while (is_word(interp->src[interp->cur_pos])) {
         if (interp->src[interp->cur_pos] == '\0') goto ERR_EOF;
 
-        word[interp->cur_pos] = interp->src[interp->cur_pos];
+        word[word_count] = interp->src[interp->cur_pos];
+        word_count++;
         interp->cur_pos++;
 
         if (interp->cur_pos >= max_size) return true;    // this is success.
     }
-
+    word[word_count] = '\0';
     return true;
 
     // set interp->errno.
